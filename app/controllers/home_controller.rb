a72fdf06f56293231d5ec6fc36921e5665b292c8
samuel <samuelgarciacompanys@gmail.com>
2019-09-23 12:33:03 +0200

    user can add posts from the index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!, :only => [:index, :new]
   def index
     @posts = Post.all
+    @post = Post.new
   end
   def new
   end
