51a44ebee9a5fc5e1522b9d69196ee9006ac9f3d
samuel <samuelgarciacompanys@gmail.com>
2019-10-02 13:43:31 +0200

    fixed conditions views

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -15,5 +15,5 @@ class Friendship < ApplicationRecord
   scope :friends_active, ->(user) { where('status = (?) = ', true).where('user_id = (?)', user) }
   scope :friends_passive, ->(user) { where('status = (?) = ', true).where('friend_id = (?)', user) }
   scope :act_pas_friends?, ->(user, friend) { where('user_id = (?)', user).where('friend_id = (?)', friend).where('status=(?)', true) }
-  scope :accepted_friendships, ->(user) { where('user_id = :id OR friend_id = :id', id: user.id).where('status = (?)', true) }
+  scope :accepted_friendships, ->(user, friend) { where('user_id = :id and friend_id = :id2 OR user_id = :id2 and friend_id = :id', id: user.id, id2: friend.id).where('status = (?)', true) }
 end
