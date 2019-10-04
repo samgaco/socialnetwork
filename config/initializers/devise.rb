9209a17c1c143edd42cc5eb81750e386391a6b57
samuel <samuelgarciacompanys@gmail.com>
2019-10-04 14:30:41 +0200

    let's see2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -263,7 +263,7 @@ Devise.setup do |config|
   config.omniauth :facebook, "app id", "app secret", scope: 'email', info_fields: 'email'
 
   config.omniauth :facebook, '2393418607536464', '84b66956b12ef882dff9407ae37ffb5e', scope: 'email', info_fields: 'email',
-  callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  callback_url: "https://pentalingo.herokuapp.com/users/auth/facebook/callback"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
